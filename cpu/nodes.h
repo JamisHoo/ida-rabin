f04375eee84fffe5bd3eb07ba0d377e82295f317
Jamis Hoo <hoojamis@gmail.com>
2015-12-25 10:54:39 +0800

    common variables for both server and client

---
+++ cpu/nodes.h
@@ -0,0 +1,15 @@
+#ifndef NODES_H_
+#define NODES_H_
+
+
+#define NUM_CLIENTS (2)
+
+int server_port = 5000;
+char* server_ip = "127.0.0.1";
+
+
+#define DATA_SIZE (1<<24)
+#define COLUMN (64)
+
+
+#endif /* NODES_H_ */
